583ed10745080d810ec3839a10808aee1eccf0a2
jrrodrigo42 <rjr89000@gmail.com>
2024-04-28 15:59:18 -0300

    feat: add button "ver mas"

--- src/components/professionalList.tsx
+++ src/components/professionalList.tsx
@@ -1,109 +1,3 @@
-// // src/components/ProfessionalList.tsx
-// import React, { useState, useEffect } from 'react';
-// import { Professional } from '../models/professional';
-// import { getProfessionals, createProfessional } from '../services/professionalService';
-
-// const ProfessionalList: React.FC = () => {
-//   const [professionals, setProfessionals] = useState<Professional[]>([]);
-  
-//   const [newProfessional, setNewProfessional] = useState<Professional>({
-//     name: '',
-//     category: '',
-//     location: '',
-//     availability: [], 
-//   });
-
-//   useEffect(() => {
-//     const fetchProfessionals = async () => {
-//       try {
-//         const fetchedProfessionals = await getProfessionals();
-//         console.log(fetchProfessionals);
-        
-//         setProfessionals(fetchedProfessionals);
-//       } catch (error) {
-//         console.error('Erro ao buscar profissionais:', error);
-//       }
-//     };
-
-//     fetchProfessionals();
-//   }, []);
-
-  
-//   const handleCreateProfessional = async (newProfessional: Professional) => {
-//     try {
-//       const createdProfessional = await createProfessional(newProfessional);
-//       setProfessionals([...professionals, createdProfessional]); // Atualiza a lista
-//       return <p>Profissional criado com sucesso!</p>; 
-//     } catch (error) {
-//       console.error('Erro ao criar profissional:', error);
-//       return <p>Erro ao criar profissional!</p>; 
-//     }
-//   };
-
-//   return (
-//     <div>
-      
-//       <div>
-//       <h1>Lista de Profissionais</h1>
-//       <ul>
-//         {professionals.map((professional) => (
-//           <li className='p-4'>
-//             <strong>Nome:</strong> {professional.name} <br />
-//             <strong>Categoria:</strong> {professional.category} <br />
-//             <strong>Localização:</strong> {professional.location}
-//           </li>
-//         ))}
-//       </ul>
-//       </div>
-    
-      
-//       <form
-//   onSubmit={(event) => {
-//     event.preventDefault();
-//     const createdProfessional = {
-//       name: newProfessional.name,
-//       category: newProfessional.category,
-//       location: newProfessional.location,
-//       availability: newProfessional.availability,
-//     };
-//     handleCreateProfessional(createdProfessional);
-//   }}
-// >
-
-//   <input className='p-2'
-//     type="text"
-//     placeholder="Nome"
-//     value={newProfessional.name}
-//     onChange={(e) =>
-//       setNewProfessional({ ...newProfessional, name: e.target.value })
-//     }
-//   />
-//   <input
-//     type="text"
-//     placeholder="Categoria"
-//     value={newProfessional.category}
-//     onChange={(e) =>
-//       setNewProfessional({ ...newProfessional, category: e.target.value })
-//     }
-//   />
-//   <input
-//     type="text"
-//     placeholder="Localização"
-//     value={newProfessional.location}
-//     onChange={(e) =>
-//       setNewProfessional({ ...newProfessional, location: e.target.value })
-//     }
-//   />
- 
-//   <button className='bg-slate-300 rounded-2xl' type="submit">Criar Profissional</button>
-// </form>
-//     </div>
-//   );
-// };
-
-// export default ProfessionalList;
-
-
 import React, { useState, useEffect } from 'react';
 import { Professional } from '../models/professional';
 import { getProfessionals, createProfessional } from '../services/professionalService';
@@ -220,7 +114,8 @@ const ProfessionalList: React.FC = () => {
           <li  className="p-8 border-b">
             <strong>Nome:</strong> {professional.name} <br />
             <strong>Categoria:</strong> {professional.category} <br />
-            <strong>Localização:</strong> {professional.location}
+            <strong>Localização:</strong> {professional.location} <br />
+            <button className='bg-violet-700'>Ver mais</button> 
           </li>
         )).reverse()}
       </ul>
